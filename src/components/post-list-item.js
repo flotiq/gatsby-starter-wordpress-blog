ada1ab40897823105473771b6713892c96fd9746
michal.chrzaszcz <mchrzaszcz90@gmail.com>
2021-02-01 12:17:35 +0100

    Style updates for homepage and lists

--- src/components/post-list-item.js
+++ src/components/post-list-item.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
 import React from "react"
+import trim from "../utils/trim";
 
 const PostListItem = ({ node, latest }) => {
   const title = node.title || node.slug
@@ -29,7 +30,7 @@ const PostListItem = ({ node, latest }) => {
       <div className="col-sm-6 post-list-image">
         <Link style={{ boxShadow: `none` }} to={`/post/${node.slug}`}>
           { node.featuredMedia && node.featuredMedia[0] &&
-          <img src={`https://api.flotiq.com/image/460x0/${node.featuredMedia[0].id}.${node.featuredMedia[0].extension}`} alt="test" style={{maxWidth: '100%', height: 'auto'}}/>
+          <img src={`https://api.flotiq.com/image/460x300/${node.featuredMedia[0].id}.${node.featuredMedia[0].extension}`} alt="test" style={{maxWidth: '100%', height: 'auto'}}/>
           }
         </Link>
       </div>
@@ -40,18 +41,18 @@ const PostListItem = ({ node, latest }) => {
           <h3
             className="post-list-name"
             style={{
-              marginTop: rhythm(1 / 2),
-              marginBottom: rhythm(1 / 2),
+              marginTop: rhythm(1 / 3),
+              marginBottom: rhythm(1 / 3),
             }}
           >
             <Link style={{ boxShadow: `none` }} to={`/post/${node.slug}`}>
-              {title}
+                {latest ? trim(title) : title}
             </Link>
           </h3>
           <small className="post-list-date">{(new Date(node.created ?? node.flotiqInternal.createdAt)).toDateString()}</small>
         </header>
         {!latest && <section>
-          <p
+          <p className="post-list-content"
             dangerouslySetInnerHTML={{
               __html: node.description || node.excerpt,
             }}
